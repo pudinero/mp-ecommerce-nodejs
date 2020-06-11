4070f6d6deeab501e50fdfcf60f03e8bb3f99a13
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-06-10 23:43:10 -0300

    Integración final API Mercado Pago

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mercadopago.configure({
     sandbox: true,
-    access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"
+    access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004' 
 });
-  
+
+const HEROKU_URL = "https://pudinero-mp-commerce-nodejs.herokuapp.com/"
+
 var preference = {
     items: [
         {
+            id: 1234,
             title: "Item",
-            description: "Description",
+            description: "Dispositivo móvil de Tienda e-commerce",
             quantity: 1,
             currency_id: "ARS",
             unit_price: 10,
@@ -24,11 +28,17 @@ var preference = {
         }
     ],
     payer: {
-      email: 'test_user_63274575@testuser.com',
-      address: {
-          zip_code: '1111',
-          street_name: 'False',
-          street_number: 123
+        name: 'Lalo',
+        surname: 'Landa',
+        email: 'test_user_63274575@testuser.com',
+        phone: {
+            area_code: '11',
+            number: 22223333
+        },
+        address: {
+            zip_code: '1111',
+            street_name: 'False',
+            street_number: 123
       }
     },
     payment_methods: {
@@ -39,58 +49,98 @@ var preference = {
         ],
         excluded_payment_types: [],
         installments: 6
-    }
+    },
+    external_reference: 'cristianleguizamon37@gmail.com',
+    back_urls: {
+        success: HEROKU_URL + '/payment/success',
+        pending: HEROKU_URL + '/payment/pending',
+        failure: HEROKU_URL + '/payment/failure'
+    },
+    auto_return: 'approved'
 }
 
-/*
-TestPreference.items[0].title = "Item 1 (modified)"
-
-mercadopago.preferences.create(TestPreference).then(function (data) {
-    console.log(data)
-}).catch(function (error) {
-    console.log(error)
-});*/
+//console.log(mercadopago)
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+    req.query.view = "home"
+    res.render('home', req.query);
 });
 
 app.get('/detail', function (req, res) {
+
+    req.query.view = "item"
+
     preference.items[0].title = req.query.title;
     preference.items[0].description = req.query.title;
     preference.items[0].unit_price = parseFloat(req.query.price);
     preference.items[0].quantity = parseInt(req.query.unit);
-    preference.items[0].picture_url = "https://pudinero-mp-commerce-nodejs.herokuapp.com/" + req.query.img.slice(2);
+    preference.items[0].picture_url = HEROKU_URL + req.query.img.slice(2);
+
+    console.log(preference.payer)
 
     mercadopago.preferences.create(preference).then(function (data) {
-        req.query.id = data.body.id;
-        console.log(req.query.id)
+        req.query.init_point = data.body.init_point;
+        console.log(data.body.init_point)
 
         res.render('detail', req.query);
 
     }).catch(function (error) {
         console.log(error)
     });
+
 });
 
-app.post('/procesar-pago', function (req, res) {
-    //console.log(req.body);
+app.get('/payment/:status', async (req, res, next) => {
+    
+    var rawUrl = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+    const search_params = rawUrl.searchParams;
 
-    mercadopago.getPayment(req.body.payment_id).then(function (data) {
-        //req.query.id = data.body.id;
-        console.log(data)
+    console.log(search_params)
 
-        res.send(data)
+    switch (req.params.status) {
+        case 'success':
 
-    }).catch(function (error) {
-        console.log(error)
-    });
+            /*mercadopago.payment.get(search_params.get('collection_id')).then(function (data) {
+                res.send(data)
+            }).catch(function (error) {
+                console.log(error)
+            });*/
 
-    //res.send("Compra finalizada");
-})
+            res.send(
+                {
+                    collection_id: search_params.get('collection_id'),
+                    collection_status: search_params.get('collection_status'),
+                    external_reference: search_params.get('external_reference'),
+                    payment_type: search_params.get('payment_type'),
+                    merchant_order_id: search_params.get('merchant_order_id'),
+                    preference_id: search_params.get('preference_id'),
+                    site_id: search_params.get('site_id'),
+                    processing_mode: search_params.get('processing_mode'),
+                    merchant_account_id: search_params.get('merchant_account_id')
+                });
+
+            break;
+
+        case 'failure':
+            
+            res.send("El pago no ha sido finalizado o ha sido rechazado <br /> <a href=\'/\'>Ir al inicio</a>")
+
+            break;
+
+        case 'pending':
+
+            res.send("El pago está pendiente <br /> <a href=\'/\'>Ir al inicio</a>")
+            
+            break;
+    
+        default:
+            res.send(req.params.status + ' is not a valid get request')
+            break;
+    }
+});
 
 app.use(express.static('assets'));
  
